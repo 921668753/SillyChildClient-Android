17534e734e2cb17f591501524ad7064cf7cae546
921668753 <921668753@qq.com>
2018-05-04 17:08:29 +0800

    fix BUG

--- SillyChildMerchant/app/src/main/java/com/yinglan/scm/main/MainActivity.java
+++ SillyChildMerchant/app/src/main/java/com/yinglan/scm/main/MainActivity.java
@@ -1,14 +1,409 @@
 package com.yinglan.scm.main;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import com.yinglan.scm.R;
+import android.app.Notification;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.SystemClock;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.common.cklibrary.common.BaseActivity;
+import com.common.cklibrary.common.BaseFragment;
+import com.common.cklibrary.common.BindView;
+import com.common.cklibrary.common.KJActivityStack;
+import com.common.cklibrary.common.ViewInject;
+import com.kymjs.common.Log;
+import com.kymjs.common.StringUtils;
+import com.yinglan.scm.R;
+import com.yinglan.scm.constant.StringNewConstants;
+
+
+import cn.jpush.android.api.BasicPushNotificationBuilder;
+import cn.jpush.android.api.JPushInterface;
+
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends BaseActivity implements MainContract.View  {
+
+    @BindView(id = R.id.bottombar_homePage, click = true)
+    private LinearLayout bottombar_homePage;
+
+    @BindView(id = R.id.img_homePage)
+    private ImageView img_homePage;
+
+    @BindView(id = R.id.tv_homePage)
+    private TextView tv_homePage;
+
+    @BindView(id = R.id.bottombar_message, click = true)
+    private RelativeLayout bottombar_message;
+
+    @BindView(id = R.id.tv_messageTag)
+    public TextView tv_messageTag;
+
+    @BindView(id = R.id.img_message)
+    private ImageView img_message;
+
+    @BindView(id = R.id.tv_message)
+    private TextView tv_message;
+
+    @BindView(id = R.id.bottombar_mall, click = true)
+    private LinearLayout bottombar_mall;
+
+    @BindView(id = R.id.img_mall)
+    private ImageView img_mall;
+
+    @BindView(id = R.id.tv_mall)
+    private TextView tv_mall;
+
+    @BindView(id = R.id.bottombar_trip, click = true)
+    private LinearLayout bottombar_trip;
+
+    @BindView(id = R.id.img_trip)
+    private ImageView img_trip;
+
+    @BindView(id = R.id.tv_trip)
+    private TextView tv_trip;
+
+    @BindView(id = R.id.bottombar_mine, click = true)
+    private LinearLayout bottombar_mine;
+
+    @BindView(id = R.id.img_mine)
+    private ImageView img_mine;
+
+    @BindView(id = R.id.tv_mine)
+    private TextView tv_mine;
+
+
+    private BaseFragment contentFragment;
+    private BaseFragment contentFragment1;
+    private BaseFragment contentFragment2;
+    private BaseFragment contentFragment3;
+    private BaseFragment contentFragment4;
+    private long firstTime = 0;
+
+
+    private MessageReceiver mMessageReceiver;
+    public static final String MESSAGE_RECEIVED_ACTION = "com.example.jpushdemo.MESSAGE_RECEIVED_ACTION";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_MESSAGE = "message";
+    public static final String KEY_EXTRAS = "extras";
+
+    /**
+     * 用来表示移动的Fragment
+     */
+    private int chageIcon;
+    public static boolean isForeground = true;
+    private Thread thread = null;
+    private Intent intentservice;
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void setRootView() {
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        mPresenter = new MainPresenter(this);
+//        contentFragment = new HomePageFragment();
+//        contentFragment1 = new MessageFragment();
+//        contentFragment2 = new MallFragment();
+//        contentFragment3 = new TripFragment();
+//        contentFragment4 = new MineFragment();
+        chageIcon = getIntent().getIntExtra("chageIcon", 0);
+        registerMessageReceiver();  //   极光推送 used for receive msg
+      //  ((MainContract.Presenter) mPresenter).getChatManagerListener();
+
+    }
+
+    @Override
+    public void initWidget() {
+        super.initWidget();
+        boolean isShow = getIntent().getBooleanExtra("isShow", false);
+        if (isShow) {
+            tv_messageTag.setVisibility(View.VISIBLE);
+        }
+        initColors();
+    }
+
+    public void changeFragment(BaseFragment targetFragment) {
+        super.changeFragment(R.id.main_content, targetFragment);
+    }
+
+    /**
+     * Activity的启动模式变为singleTask
+     * 调用onNewIntent(Intent intent)方法。
+     * Fragment调用的时候，一定要在onResume方法中。
+     *
+     * @param intent
+     */
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        int newChageIcon = intent.getIntExtra("newChageIcon", 4);
+        Log.d("newChageIcon", newChageIcon + "");
+        if (newChageIcon == 0) {
+            setSimulateClick(bottombar_homePage, 160, 100);
+        } else if (newChageIcon == 1) {
+            setSimulateClick(bottombar_message, 160, 100);
+        } else if (newChageIcon == 2) {
+            setSimulateClick(bottombar_mall, 160, 100);
+        } else if (newChageIcon == 3) {
+        }
+    }
+
+    /**
+     * 模拟点击
+     *
+     * @param view
+     * @param x
+     * @param y
+     */
+    private void setSimulateClick(View view, float x, float y) {
+        long downTime = SystemClock.uptimeMillis();
+        final MotionEvent downEvent = MotionEvent.obtain(downTime, downTime,
+                MotionEvent.ACTION_DOWN, x, y, 0);
+        downTime += 1000;
+        final MotionEvent upEvent = MotionEvent.obtain(downTime, downTime,
+                MotionEvent.ACTION_UP, x, y, 0);
+        view.onTouchEvent(downEvent);
+        view.onTouchEvent(upEvent);
+        downEvent.recycle();
+        upEvent.recycle();
+    }
+
+    @Override
+    public void widgetClick(View v) {
+        super.widgetClick(v);
+        switch (v.getId()) {
+            case R.id.bottombar_homePage:
+                cleanColors(0);
+                break;
+            case R.id.bottombar_message:
+                cleanColors(1);
+                break;
+            case R.id.bottombar_mall:
+                cleanColors(2);
+                break;
+            case R.id.bottombar_trip:
+                cleanColors(3);
+                break;
+            case R.id.bottombar_mine:
+                cleanColors(4);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    /**
+     * 退出应用
+     *
+     * @param keyCode
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                long secondTime = System.currentTimeMillis();
+                if (secondTime - firstTime > 2000) {
+                    //如果两次按键时间间隔大于2秒，则不退出
+//                    Toast.makeText(this,"111111",Toast.LENGTH_SHORT).show();
+                    ViewInject.toast(this, "再按一次退出程序");
+                    firstTime = secondTime;//更新firstTime
+                    return true;
+                } else {
+                    //  int i = 1 / 0;
+                    //   KjBitmapUtil.getInstance().getKjBitmap().cleanCache();
+                    //    MobclickAgent.onProfileSignOff();//关闭账号统计     退出登录也加
+                    JPushInterface.stopCrashHandler(getApplication());//JPush关闭CrashLog上报
+                    //    MobclickAgent.onKillProcess(aty);
+                    //第一个参数为是否解绑推送的devicetoken
+                    KJActivityStack.create().appExit(aty);
+                }
+                break;
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        isForeground = false;
+        unregisterReceiver(mMessageReceiver);
+        super.onDestroy();
+    }
+
+    public void registerMessageReceiver() {
+        mMessageReceiver = new MessageReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
+        filter.addAction(MESSAGE_RECEIVED_ACTION);
+        Intent intent = registerReceiver(mMessageReceiver, filter);
+        //极光推送 定制声音、震动、闪灯等 Notification 样式。
+        BasicPushNotificationBuilder builder = new BasicPushNotificationBuilder(MainActivity.this);
+//        builder.statusBarDrawable = R.mipmap.ic_launcher;
+        builder.notificationFlags = Notification.FLAG_AUTO_CANCEL
+                | Notification.FLAG_SHOW_LIGHTS;  //设置为自动消失和呼吸灯闪烁
+        builder.notificationDefaults = Notification.DEFAULT_SOUND
+                | Notification.DEFAULT_VIBRATE
+                | Notification.DEFAULT_LIGHTS;  // 设置为铃声、震动、呼吸灯闪烁都要
+        JPushInterface.setPushNotificationBuilder(1, builder);
+    }
+
+    public static class MessageReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            android.util.Log.d("JPush", "JPush1");
+            if (MESSAGE_RECEIVED_ACTION.equals(intent.getAction())) {
+                String messge = intent.getStringExtra(KEY_MESSAGE);
+                String extras = intent.getStringExtra(KEY_EXTRAS);
+                StringBuilder showMsg = new StringBuilder();
+                showMsg.append(KEY_MESSAGE + " : " + messge + "\n");
+                if (!StringUtils.isEmpty(extras)) {
+                    showMsg.append(KEY_EXTRAS + " : " + extras + "\n");
+                }
+                android.util.Log.d("JPush", "JPush");
+                //  setCostomMsg(showMsg.toString());
+            }
+        }
+    }
+
+
+    /**
+     * 清除颜色，并添加颜色
+     */
+    @SuppressWarnings("deprecation")
+    public void cleanColors(int position) {
+//        if (position!=chageIcon){
+//            switch (chageIcon){
+//                case 0:
+//                    img_homePage.setImageResource(R.mipmap.tab_home);
+//                    tv_homePage.setTextColor(getResources().getColor(R.color.textColor));
+//                    break;
+//                case 1:
+//                    img_message.setImageResource(R.mipmap.tab_message);
+//                    tv_message.setTextColor(getResources().getColor(R.color.textColor));
+//                    break;
+//                case 2:
+//                    img_mall.setImageResource(R.mipmap.tab_store);
+//                    tv_mall.setTextColor(getResources().getColor(R.color.textColor));
+//                    break;
+//                case 3:
+//                    img_trip.setImageResource(R.mipmap.tab_travel);
+//                    tv_trip.setTextColor(getResources().getColor(R.color.textColor));
+//                    break;
+//                case 4:
+//                    img_mine.setImageResource(R.mipmap.tab_personal);
+//                    tv_mine.setTextColor(getResources().getColor(R.color.textColor));
+//                    break;
+//            }
+//            chageIcon=position;
+//            switch (chageIcon){
+//                case 0:
+//                    img_homePage.setImageResource(R.mipmap.tab_home_selected);
+//                    tv_homePage.setTextColor(getResources().getColor(R.color.greenColors));
+//                    changeFragment(contentFragment);
+//                    break;
+//                case 1:
+//                    img_message.setImageResource(R.mipmap.tab_message_selected);
+//                    tv_message.setTextColor(getResources().getColor(R.color.greenColors));
+//                    changeFragment(contentFragment1);
+//                    break;
+//                case 2:
+//                    img_mall.setImageResource(R.mipmap.tab_store_selected);
+//                    tv_mall.setTextColor(getResources().getColor(R.color.greenColors));
+//                    changeFragment(contentFragment2);
+//                    break;
+//                case 3:
+//                    img_trip.setImageResource(R.mipmap.tab_travel_selected);
+//                    tv_trip.setTextColor(getResources().getColor(R.color.greenColors));
+//                    changeFragment(contentFragment3);
+//                    break;
+//                case 4:
+//                    img_mine.setImageResource(R.mipmap.tab_personal_selected);
+//                    tv_mine.setTextColor(getResources().getColor(R.color.greenColors));
+//                    changeFragment(contentFragment4);
+//                    break;
+//            }
+ //       }
+
+    }
+
+    /**
+     * 清除颜色，并添加颜色
+     */
+    @SuppressWarnings("deprecation")
+    public void initColors() {
+//        switch (chageIcon){
+//            case 0:
+//                img_homePage.setImageResource(R.mipmap.tab_home_selected);
+//                tv_homePage.setTextColor(getResources().getColor(R.color.greenColors));
+//                changeFragment(contentFragment);
+//                break;
+//            case 1:
+//                img_message.setImageResource(R.mipmap.tab_message_selected);
+//                tv_message.setTextColor(getResources().getColor(R.color.greenColors));
+//                changeFragment(contentFragment1);
+//                break;
+//            case 2:
+//                img_mall.setImageResource(R.mipmap.tab_store_selected);
+//                tv_mall.setTextColor(getResources().getColor(R.color.greenColors));
+//                changeFragment(contentFragment2);
+//                break;
+//            case 3:
+//                img_trip.setImageResource(R.mipmap.tab_travel_selected);
+//                tv_trip.setTextColor(getResources().getColor(R.color.greenColors));
+//                changeFragment(contentFragment3);
+//                break;
+//            case 4:
+//                img_mine.setImageResource(R.mipmap.tab_personal_selected);
+//                tv_mine.setTextColor(getResources().getColor(R.color.greenColors));
+//                changeFragment(contentFragment4);
+//                break;
+//        }
+
+    }
+
+
+    @Override
+    public void setPresenter(MainContract.Presenter presenter) {
+        mPresenter = presenter;
+    }
+
+    @Override
+    public void getSuccess(String success, int flag) {
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    public void errorMsg(String msg, int flag) {
+
+    }
+
+
+
+    public int getChageIcon() {
+        return chageIcon;
     }
 }
---
+++ SillyChildMerchant/app/src/main/java/com/yinglan/scm/main/MainPresenter.java
@@ -0,0 +1,160 @@
+package com.yinglan.scm.main;
+
+import android.util.Log;
+
+import com.common.cklibrary.common.KJActivityStack;
+import com.common.cklibrary.common.StringConstants;
+import com.common.cklibrary.utils.JsonUtil;
+import com.common.cklibrary.utils.httputil.HttpUtilParams;
+import com.common.cklibrary.utils.httputil.ResponseListener;
+import com.hyphenate.EMMessageListener;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMConversation;
+import com.hyphenate.chat.EMMessage;
+import com.kymjs.common.PreferenceHelper;
+import com.kymjs.rxvolley.client.HttpParams;
+//import com.yinglan.scm.entity.HxUserListBean;
+//import com.yinglan.scm.entity.SystemMessageBean;
+import com.yinglan.scm.retrofit.RequestClient;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by ruitu on 2016/9/24.
+ */
+
+public class MainPresenter implements MainContract.Presenter {
+    private MainContract.View mView;
+
+    public MainPresenter(MainContract.View view) {
+        mView = view;
+        mView.setPresenter(this);
+    }
+
+
+    @Override
+    public void getSystemMessage() {
+        HttpParams httpParams = HttpUtilParams.getInstance().getHttpParams();
+//        RequestClient.getSystemMessage(httpParams, 1, new ResponseListener<String>() {
+//            @Override
+//            public void onSuccess(String response) {
+//                SystemMessageBean systemMessageBean = (SystemMessageBean) JsonUtil.getInstance().json2Obj(response, SystemMessageBean.class);
+//                if (systemMessageBean.getResult().getList() == null || systemMessageBean.getResult().getList().size() == 0) {
+//                    mView.errorMsg(response, 0);
+//                    return;
+//                }
+//                if (systemMessageBean.getResult().getUnread() > 0) {
+//                    mView.getSuccess("", 0);
+//                } else {
+//                    boolean isRefreshingHomePageFragment = PreferenceHelper.readBoolean(KJActivityStack.create().topActivity(), StringConstants.FILENAME, "isRefreshingHomePageFragment", false);
+//                    if(!isRefreshingHomePageFragment){
+//                        getGuideMessage();
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(String msg) {
+//                boolean isRefreshingHomePageFragment = PreferenceHelper.readBoolean(KJActivityStack.create().topActivity(), StringConstants.FILENAME, "isRefreshingHomePageFragment", false);
+//                if(!isRefreshingHomePageFragment){
+//                    getGuideMessage();
+//                }
+//            }
+//        });
+    }
+
+    @Override
+    public void getGuideMessage() {
+        HttpParams httpParams = HttpUtilParams.getInstance().getHttpParams();
+        httpParams.put("type", "driver");
+        httpParams.put("page", 1);
+        httpParams.put("pageSize", 1);
+//        RequestClient.getHxUserList(httpParams, new ResponseListener<String>() {
+//            @Override
+//            public void onSuccess(String response) {
+//                if (loadConversationList(response)) {
+//                    mView.getSuccess(response, 0);
+//                } else {
+//                    mView.errorMsg(response, 0);
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(String msg) {
+//                mView.errorMsg(msg, 0);
+//            }
+      //  });
+    }
+
+    @Override
+    public void getChatManagerListener() {
+        EMClient.getInstance().chatManager().addMessageListener(msgListener);
+    }
+
+    EMMessageListener msgListener = new EMMessageListener() {
+
+        @Override
+        public void onMessageReceived(List<EMMessage> messages) {
+            Log.d("tag1111", JsonUtil.obj2JsonString(messages.get(messages.size() - 1)).getBytes().toString());
+            //收到消息
+            getGuideMessage();
+        }
+
+        @Override
+        public void onCmdMessageReceived(List<EMMessage> messages) {
+            //收到透传消息
+//            refresh();
+        }
+
+        @Override
+        public void onMessageRead(List<EMMessage> messages) {
+            //收到已读回执
+        }
+
+        @Override
+        public void onMessageDelivered(List<EMMessage> message) {
+            //收到已送达回执
+        }
+
+        @Override
+        public void onMessageRecalled(List<EMMessage> messages) {
+            //消息被撤回
+            getGuideMessage();
+        }
+
+        @Override
+        public void onMessageChanged(EMMessage message, Object change) {
+            //消息状态变动
+        }
+    };
+
+    protected boolean loadConversationList(String response) {
+//        HxUserListBean hxUserListBean = (HxUserListBean) JsonUtil.getInstance().json2Obj(response, HxUserListBean.class);
+//        if (hxUserListBean == null || hxUserListBean.getResult() == null || hxUserListBean.getResult().size() == 0) {
+//            return false;
+//        }
+//        List<HxUserListBean.ResultBean> hxUserList = hxUserListBean.getResult();
+//        Map<String, EMConversation> conversations = EMClient.getInstance().chatManager().getAllConversations();
+//        synchronized (conversations) {
+//            for (EMConversation conversation : conversations.values()) {
+//                if (conversation.getAllMessages().size() != 0) {
+//                    try {
+//                        for (int i = 0; i < hxUserList.size(); i++) {
+//                            if (hxUserList.get(i).getHxName().equals(conversation.conversationId())) {
+//                                Log.d("tag1111", conversation.conversationId());
+//                                if (conversation.getUnreadMsgCount() > 0) {
+//                                    return true;
+//                                }
+//                            }
+//                        }
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                        return false;
+//                    }
+//                }
+//            }
+//        }
+        return false;
+    }
+}
