445da38591f4f492fb940d1b5b600947d0db7fda
921668753 <921668753@qq.com>
2018-05-06 17:50:57 +0800

    fix  BUG

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# SillyChildClient   傻孩子用户端（安卓）
-# SillyChildMerchant   傻孩子商家端（安卓）
+﻿# SillyChildClient   傻孩子用户端（安卓）
--- SillyChildMerchant/app/src/main/java/com/yinglan/scm/utils/easeim/domain/VideoEntity.java
+++
@@ -1,9 +0,0 @@
-package com.yinglan.scm.utils.easeim.domain;
-
-public class VideoEntity {
-	public int ID;
-	public String title;
-	public String filePath;
-	public int size;
-	public int duration;
-}
