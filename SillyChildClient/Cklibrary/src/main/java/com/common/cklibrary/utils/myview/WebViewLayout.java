a86a955c482d7b959e319e790178ceb1b08ea514
921668753 <921668753@qq.com>
2018-06-23 21:57:36 +0800

    fix BUG

--- SillyChildClient/Cklibrary/src/main/java/com/common/cklibrary/utils/myview/WebViewLayout.java
+++ SillyChildClient/Cklibrary/src/main/java/com/common/cklibrary/utils/myview/WebViewLayout.java
@@ -88,14 +88,40 @@ public class WebViewLayout extends LinearLayout {
         //添加webview
         webView = new ObservableWebView(context);
         webView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        //声明WebSettings子类
+        WebSettings webSettings = webView.getSettings();
         /**
          *  Webview在安卓5.0之前默认允许其加载混合网络协议内容
          *  在安卓5.0之后，默认不允许加载http与https混合内容，需要设置webview允许其加载混合网络协议内容
          */
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         }
-        webView.getSettings().setJavaScriptEnabled(isJavaScriptEnabled);
+        webSettings.setJavaScriptEnabled(isJavaScriptEnabled);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);  //设置 缓存模式
+        //如果访问的页面中要与Javascript交互，则webview必须设置支持Javascript
+        webSettings.setJavaScriptEnabled(true);
+        // 若加载的 html 里有JS 在执行动画等操作，会造成资源浪费（CPU、电量）
+        // 在 onStop 和 onResume 里分别把 setJavaScriptEnabled() 给设置成 false 和 true 即可
+
+        //支持插件
+        //    webSettings.setPluginsEnabled(true);
+
+        //设置自适应屏幕，两者合用
+        webSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
+        webSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
+
+        //缩放操作
+//        webSettings.setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
+//        webSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
+//        webSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
+
+        //其他细节操作
+     //   webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); //关闭webview中缓存
+        webSettings.setAllowFileAccess(true); //设置可以访问文件
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true); //支持通过JS打开新窗口
+        webSettings.setLoadsImagesAutomatically(true); //支持自动加载图片
+        webSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
         webView.setWebViewClient(new WebViewClient());
         webView.setWebChromeClient(new WebChromeClient());
         webView.setVerticalScrollBarEnabled(false); //垂直不显示
