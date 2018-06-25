368d02db1bc2e46cfa54faffb84e2a84b3db2a2d
921668753 <921668753@qq.com>
2018-06-25 17:25:32 +0800

    fix BUG

--- SillyChildClient/Cklibrary/src/main/java/com/common/cklibrary/utils/myview/WebViewLayout1.java
+++ SillyChildClient/Cklibrary/src/main/java/com/common/cklibrary/utils/myview/WebViewLayout1.java
@@ -13,7 +13,6 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.net.http.SslError;
 import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
